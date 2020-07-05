263c996a7db61e81127fc62b4cd5b2a5691c7002
Mostafa Mohsen <reachmostafa.m@gmail.com>
2020-07-05 01:03:51 -0400

    Convolution with CUDNN

---
+++ CUDNN_stuff/convolution_example.cu
@@ -0,0 +1,43 @@
+
+#include <cudnn.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#define checkCUDNN(expression)                               \
+  {                                                          \
+    cudnnStatus_t status = (expression);                     \
+    if (status != CUDNN_STATUS_SUCCESS) {                    \
+      std::cerr << "Error on line " << __LINE__ << ": "      \
+                << cudnnGetErrorString(status) << std::endl; \
+      std::exit(EXIT_FAILURE);                               \
+    }                                                        \
+  }
+
+cv::Mat ldimag(const char* image_path){
+    cv::Mat image = cv::imread(image_path);
+    image.convertTo(image, CV_32FC3);
+    cv::normalize(image, image, 0, 1, cv::NORM_MINMAX);
+    return image;
+}
+
+int main(void){
+    cudnnHandle_t cudnn;
+    checkCUDNN(cudnnCreate(&cudnn));
+    cv::Mat image = ldimag("conure.jpg");
+
+    cudnnTensorDescriptor_t input_descriptor;
+    checkCUDNN(cudnnCreateTensorDescriptor(&input_descriptor));
+    checkCUDNN(cudnnSetTensor4dDescriptor(input_descriptor, 
+                                            CUDNN_TENSOR_NCHW, 
+                                            CUDNN_DATA_FLOAT, 
+                                            1, 
+                                            3,
+                                            image.rows,
+                                            image.cols));
+
+    
+
+
+
+    return 0;
+}
